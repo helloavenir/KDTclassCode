11283c29b7a9b3db28e8db38bdc1ab9f5fd28a8f
HelloAvenir <102899881+helloavenir@users.noreply.github.com>
2022-10-04 21:57:08 +0900

    Create main.js

---
+++ 1004class/0/main.js
@@ -0,0 +1,11 @@
+console.log(123 + null);
+console.log(typeof(123 + null));
+
+console.log(123 - null);
+console.log(typeof(123 - null));
+
+console.log(123 * null);
+console.log(typeof(123 * null));
+
+console.log(123 / null);
+console.log(typeof(123 / null));
